15d05a6ae8e4f20291f9d55f4b4d5bd826c26caa
jinyoung kim <jinyoungkim@jinyoungui-MacBook-Pro.local>
2018-06-10 04:57:08 +0900

    remove spring-boot cookbook

--- cookbooks/spring-boot/libraries/matchers.rb
+++
@@ -1,7 +0,0 @@
-def install_spring_boot_web_app(name)
-  ChefSpec::Matchers::ResourceMatcher.new(:spring_boot_web_app, :install, name)
-end
-
-def uninstall_spring_boot_web_app(name)
-  ChefSpec::Matchers::ResourceMatcher.new(:spring_boot_web_app, :uninstall, name)
-end
